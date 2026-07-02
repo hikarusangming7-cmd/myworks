1c82fefb077d68420336cbe429c21e846133d100
hikarusangming7-cmd <hikarusangming7@gmail.com>
2026-07-02 18:20:03 +0900

    初めてのコミット

---
+++ main.js
@@ -0,0 +1,247 @@
+const mask = document.querySelector("#mask");
+const modal = document.querySelector("#modal");
+const close = document.querySelector(".close");
+const openButtons = document.querySelectorAll(".open");
+const workTitle = document.querySelector("#workTitle");
+const workItem = document.querySelector("#workItem");
+const workSkill = document.querySelector("#workSkill");
+const workTerm = document.querySelector("#workTerm");
+const workPoint = document.querySelector("#workPoint");
+const workUrl = document.querySelector("#workUrl");
+const workImage = document.querySelector("#workImage");
+const thumbnails = document.querySelectorAll(".work-thumb");
+const left = document.querySelector("#left");
+const right = document.querySelector("#right");
+const imagePreviewMask = document.querySelector("#imagePreviewMask");
+const imagePreviewModal = document.querySelector("#imagePreviewModal");
+const previewImage = document.querySelector("#previewImage");
+const termArea = document.querySelector("#termArea");
+const urlArea = document.querySelector("#urlArea");
+
+
+
+
+
+const works = [
+    {
+        workImage:`images/portfolio.png`,
+        workTitle: `うみぼうずはどこだ？ポートフォリオサイト`,
+        workItem: `隠れているチワワが何匹いるかを探し出すポートフォリオサイト`,
+        workSkill: `HTML/CSS/JavaScript`,
+        workTerm: `3週間`,
+        workPoint: `スクラップブック風のデザインになっています。ケージから出してほしくて震えさせたり等、愛犬らしさを意識して作りました。工夫した点は、GSAPで手書き風の文字をIllustratorで描いたり、ただのポートフォリオにならないように、楽しめる仕掛けを考えたところです。`,
+        workUrl:`https://hikarusangming7-cmd.github.io/umibozu_portfolio/`
+    },
+    {
+        workImage:`images/stmaker.png`,
+        workTitle: `物語メーカー`,
+        workItem: `ランダムで物語、キャラクター、告白の台詞、保留にしていた告白を断る台詞を生成してくれるアプリ`,
+        workSkill: `HTML/CSS/JavaScript`,
+        workTerm: `2週間`,
+        workPoint: `localStorageで保存、削除ができるようになっています。また、保存したデータを後から編集できる機能を実装しました。編集内容はブラウザに再保存され、ページを再読み込みしても保持されます。また、編集後は画面表示も即時更新されるようにしました。`,
+        workUrl:`https://hikarusangming7-cmd.github.io/Story__Maker/`
+    },
+    {
+        workImage:`images/saveanimal.png`,
+        workTitle: `Save Animals`,
+        workItem: `犬猫保護団体サイト`,
+        workSkill: `WordPress`,
+        workTerm: `1週間`,
+        workPoint: `訓練校の授業でブロックテーマで作りました。サイトを訪れるユーザーが迷わないように、わかりやすさを意識しました。`,
+        workUrl:`https://shy-iki-9326.main.jp/wordpress/`
+
+    },
+    {
+        workImage:`images/kaidan.png`,
+        workTitle: `運命怪談`,
+        workItem: `マルチエンディングホラーゲーム`,
+        workSkill: `HTML/CSS/JavaScript`,
+        workTerm: `製作中`,
+        workPoint: `シナリオはシーンデータとして配列で管理し、各シーンに一意のIDを付与しています。選択肢には遷移先の nextSceneId を持たせ、ユーザーの選択に応じて対応するシーンを表示するようにしました。`,
+        workUrl:`https://hikarusangming7-cmd.github.io/unmeikaidan/assets/`
+
+
+    },
+    {
+        workImage:`images/pokeapi.png`,
+        workTitle: `API図鑑`,
+        workItem: `APIでデータを取得して保存できるサイト`,
+        workSkill: `HTML/CSS/JavaScript`,
+        workTerm: `製作中`,
+        workPoint: `API取得とlocalStorageの練習をするために作ったサイトです。`,
+        workUrl:`https://hikarusangming7-cmd.github.io/api_practice/Pokemon/`
+
+    },
+        {
+        workImage:`images/banner.png`,
+        workTitle: `ドッグランのバナー`,
+        workSkill: `Canva`,
+        workPoint: `屋外の開放感と楽しそうな雰囲気を伝えるため、夏らしい鮮やかなイエローとヤシの木のグリーンを基調に、南国風の爽やかな世界観を目指しました。
+        最初は全部白い文字でフチをつけて見やすくしていましたが、あまり楽しそうに見えなかったため、メインの「DOG RUN」をパッと目を引く鮮やかなカラフルグラデーションに変更し、
+        続く「OPEN」をシンプルな白文字にすることで、賑やかで楽しそうな印象を与えつつ、文字が背景に埋もれずくっきりと前に飛び出してくるように工夫しました。`,
+
+    },
+        {
+        workImage:`images/cafebanner.png`,
+        workTitle: `カフェのアルバイト募集のバナー`,
+        workSkill: `Canva`,
+        workPoint: `求職者の心理に寄り添い、単なる条件羅列で終わらせないためのアクセントとして「まかないは美味しいと評判です♪」というスタッフのイラスト付き吹き出しを配置しました。
+        お店の親しみやすさや働く楽しさを演出し、ターゲットが応募への一歩を踏み出しやすくなるように意識して制作しました。`,
+
+    },
+];
+
+let currentIndex = 0;
+
+const showWork = (index) =>{
+    currentIndex = index; //const index = Number(button.dataset.index);
+
+    const work = works[currentIndex]; 
+
+    workImage.src = work.workImage;
+    workTitle.textContent = work.workTitle;
+    workItem.textContent = work.workItem || "";
+    workSkill.textContent = work.workSkill;
+    workTerm.textContent = work.workTerm || "";
+    workPoint.textContent = work.workPoint || "";
+
+    if (work.workUrl) {
+        workUrl.textContent = work.workUrl;
+        workUrl.href = work.workUrl.replace("URL：", "");
+    } else {
+        workUrl.textContent = "";
+        workUrl.href = "#";
+    }
+    //replaceは、URL:の文字とURLをわけるために書いている。文字列.replace("置き換えたい文字", "新しい文字")
+    //const text = "こんにちは";
+// console.log(text.replace("にち", ""));
+
+// ↓
+
+// こんは
+
+// になる
+
+    if(work.workSkill === "Canva"){
+        workImage.classList.add("is-banner");
+        termArea.style.display = "none";
+        urlArea.style.display = "none";
+
+        workTitle.style.cursor = "pointer";
+        workTitle.style.textDecoration = "underline";
+
+        workTitle.onclick = () => {
+            previewImage.src = work.workImage; // 拡大用モーダルに画像セット
+            imagePreviewMask.classList.remove("hidden");
+            imagePreviewModal.classList.remove("hidden");
+        };
+    }else{
+        workImage.classList.remove("is-banner");
+        termArea.style.display = "block";
+        urlArea.style.display = "block";
+
+        workTitle.style.cursor = "default";
+        workTitle.style.textDecoration = "none";
+        workTitle.onclick = null;
+    }
+
+}
+
+const openModal = () => {
+        modal.classList.remove("hidden");
+        mask.classList.remove("hidden");
+};
+
+const closeModal = () => {
+    modal.classList.add("hidden");
+    mask.classList.add("hidden");
+};
+
+
+openButtons.forEach((button)=>{
+    button.addEventListener("click",()=>{
+        const index = Number(button.dataset.index);// HTMLで、data-indexと書くと、button.dataset.indexで取得できる。datasetで取得した値は文字列なので、Stringと表示されてしまう。配列の番号で取得したいので、数字に変換している。
+
+        showWork(index);
+        openModal();
+
+    })
+
+})//複数形だとそのままaddEventListener使えないので、forEachの中で使う。
+
+
+
+// openButtons.addEventListener("click",()=>{
+//     modal.classList.remove("hidden");
+//     mask.classList.remove("hidden");
+//     if(close.addEventListener("click",()=>{
+//         modal.classList.add("hidden");
+//         mask.classList.add("hidden");
+//     }));
+// });
+
+close.addEventListener("click", () => {
+    closeModal();
+});
+
+mask.addEventListener("click", () => {
+    closeModal();
+});
+
+right.addEventListener("click",()=>{
+    currentIndex++;
+    if(currentIndex >= works.length){
+        currentIndex = 0;
+    }
+
+    showWork(currentIndex);
+})
+
+left.addEventListener("click", () => {
+    currentIndex--;
+    if(currentIndex < 0){
+        currentIndex = works.length -1;
+    }
+
+    showWork(currentIndex);
+})
+
+
+imagePreviewMask.addEventListener("click", () => {
+    imagePreviewMask.classList.add("hidden");
+    imagePreviewModal.classList.add("hidden");
+});
+
+
+
+
+
+
+
+const captions = document.querySelectorAll(".caption");
+
+captions.forEach((caption, index) => {
+    caption.style.cursor = "pointer";
+
+    caption.addEventListener("click", (e) => {
+        const work = works[index];
+
+        if (work.workSkill === "Canva") {
+            e.preventDefault(); 
+            previewImage.src = work.workImage; 
+            imagePreviewMask.classList.remove("hidden");
+            imagePreviewModal.classList.remove("hidden");
+        } else {
+            showWork(index);
+            openModal();
+        }
+    });
+});
+
+openButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const index = Number(button.dataset.index);
+        showWork(index);
+        openModal();
+    });
+});
