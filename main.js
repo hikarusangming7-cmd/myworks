ee2de00427ddb13957227030a22fe3f198acc985
hikarusangming7-cmd <hikarusangming7@gmail.com>
2026-07-06 22:36:05 +0900

    チョコレートバナー修正

--- main.js
+++ main.js
@@ -92,7 +92,7 @@ const works = [
 
     },
             {
-        workImage:`images/chocolanoirbanner76.png`,
+        workImage:`images/noir.png`,
         workTitle: `新作チョコレートの広告バナー(300 × 250px)`,
         workItem: `30代〜50代の、普段がんばっているご褒美が必要な女性`,
         workSkill: `Canva`,
